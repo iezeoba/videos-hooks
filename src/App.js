4fc2679bb4a3055161f91447eb5562380220a421
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2021-01-26 15:55:30 -0700

    refactored codes for reusability

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+//coding the videos app using hooks//
+import React, { useState, useEffect } from "react";
+//import youtube from "./api/youtube";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+import VideoDetails from "./components/VideoDetails";
+import useVideos from "./hooks/useVideos";
 
-function App() {
+const App = () => {
+  // const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, onSearchQuerySubmit] = useVideos("football");
+
+  // useEffect(() => {
+  //   onSearchQuerySubmit("football");
+  // }, []);
+
+  // const onSearchQuerySubmit = async (searchquery) => {
+  //   const response = await youtube.get("/search", {
+  //     params: {
+  //       q: searchquery,
+  //     },
+  //   });
+  //   setVideos(response.data.items);
+  //   setSelectedVideo(response.data.items[0]);
+  // };
+
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      <SearchBar onSearchQuerySubmit={onSearchQuerySubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetails video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList
+              videos={videos}
+              videoSelect={(video) => setSelectedVideo(video)}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
